c7d4d3934e3bf5fc8135bf9f71b99a792db68c5c
root <root@z247.mikr.us>
2024-08-07 19:26:35 +0000

    Rework config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
     $env = parse_ini_file('.env');
 
     // mysqli_connect(host, username, password, dbname, port, socket)
-    $conn = mysqli_connect("localhost", "root", "", $env["DB_NAME"]);
+    $conn = mysqli_connect("localhost", $env["DB_USER"], $env["DB_PASS"], $env["DB_NAME"]);
     if (!$conn) {
         die("Error connecting to the database" . mysqli_connect_error());
     }
